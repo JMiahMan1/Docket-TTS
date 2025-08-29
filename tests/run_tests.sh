d3e76c3f1018711bde2d29d5aaf356008b4c7f97
jeremiah <jeremiah@ai.sumemail.com>
2025-08-28 17:49:08 -0700

    Download Translate Module

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,6 +8,16 @@ echo "--- Installing test dependencies ---"
 # Install app dependencies from requirements.txt and the pytest package
 pip install --no-cache-dir -r requirements.txt pytest Flask gunicorn celery redis python-docx EbookLib PyMuPDF beautifulsoup4 inflect mutagen argostranslate requests
 
+echo "--- Downloading Argos Translate model for testing ---"
+# This step is crucial to ensure the test environment has the required model
+python -c "\
+from argostranslate import package;\
+package.update_package_index();\
+available_packages = package.get_available_packages();\
+package_to_install = next(filter(lambda x: x.from_code == 'he' and x.to_code == 'en', available_packages));\
+package.install_from_path(package_to_install.download());\
+"
+
 # Check if a custom URL is provided, otherwise use the default
 if [ -z "$APP_BASE_URL" ]; then
   export APP_BASE_URL="http://localhost:8000"
